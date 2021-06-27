6fe264d0b7203ac20c0c66e152ec54d3dd3bcdda
Admin <duneesha.2017336@Iiit.ac.lk>
2021-06-27 12:34:28 +0530

    Factory Design Pattern

--- src/CoderByte/FactoryMain.java
+++ src/CoderByte/FactoryMain.java
@@ -3,6 +3,7 @@ package CoderByte;
 public class FactoryMain {
 
     public static void main(String[] args) {
+
         OperatingSystemFactory osf = new OperatingSystemFactory();
         OS obj = osf.getInstance("Open");
         obj.spec();
---
+++ src/FactoryDesign/Rectangle.java
@@ -0,0 +1,8 @@
+package FactoryDesign;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println(" Inside the Rectangle");
+    }
+}
